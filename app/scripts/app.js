767e71b3aa74ccf404602062fef963cb60b1ed48
Bo Li <llfive@hotmail.com>
2016-06-24 11:22:54 -0400

    test

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -7,3 +7,42 @@
  *
  * Main module of the application. Define the routing architecture for the app
  */
+
+var routerApp = angular.module('routerApp', ['ui.router']);
+
+routerApp.config(function($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/root');
+
+    $stateProvider
+
+        .state('root', {
+            url: '/root',
+            templateUrl: '../templates/root.html'
+        })
+
+        .state('root.work', {
+            url: '/work',
+            templateUrl: '../templates/work.html'
+        })
+
+        .state('root.overview', {
+            url: '/overview',
+            templateUrl: '../templates/overview.html'
+        })
+
+        .state('root.contact', {
+            url: '/contact',
+            templateUrl: '../templates/contact.html'
+        })
+
+        .state('root.producer', {
+            url: '/producer',
+            templateUrl: '../templates/producer.html'
+        })
+
+        .state('login', {
+            url: '/login',
+            templateUrl: '../templates/login.html'
+        })
+});
--- app/scripts/controllers/work.js
+++ app/scripts/controllers/work.js
@@ -2,3 +2,10 @@
 // WORK CONTROLLER
 // Description: Define the following functionalities:
 // All the functionalities on the "Work" section to display items and filter them
+
+routerApp.controller('workController',function($scope, $http){
+    $http.get('/api/work')
+        .success(function(data) {
+            $scope.myData = data;
+        })
+});
